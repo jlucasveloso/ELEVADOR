7d47e95ba4fa1b3fba3b3fc0799a2b125ffebc8b
J Lucas Veloso <jlvelosofurtado2@gmail.com>
2025-05-13 10:54:16 -0300

    SimuladorElevador.java

--- SimuladorElevador.java
+++ SimuladorElevador.java
@@ -1,18 +1,48 @@
+import java.util.Random;
+
 public class SimuladorElevador {
     public static void main(String[] args) {
         Configuracao config = new Configuracao();
-        Elevador elevador = new Elevador(1, config);
+        Elevador[] elevadores = new Elevador[2];
+        for (int i = 0; i < 2; i++) {
+            elevadores[i] = new Elevador(i + 1, config);
+        }
 
-        Pessoa p1 = new Pessoa("João", 65, false, 80, 5); // Idoso
-        Pessoa p2 = new Pessoa("Maria", 30, true, 70, 7); // Cadeirante
-        Pessoa p3 = new Pessoa("Carlos", 25, false, 75, 3); // Adulto
+        gerarPessoasAutomaticamente(elevadores);
+        
+        for (Elevador elevador : elevadores) {
+            System.out.println("\n=== Elevador " + elevador.getId() + " iniciando atendimento ===");
+            while (elevador.temPessoas()) {
+                elevador.atenderProximo();
+            }
+        }
+    }
 
-        elevador.adicionarPessoa(p1);
-        elevador.adicionarPessoa(p2);
-        elevador.adicionarPessoa(p3);
+    public static void gerarPessoasAutomaticamente(Elevador[] elevadores) {
+        Random random = new Random();
+        int pessoasAdicionadas = 0;
 
-        elevador.atenderProximo(); // Atende Maria
-        elevador.atenderProximo(); // Atende João
-        elevador.atenderProximo(); // Atende Carlos
+        while (pessoasAdicionadas < 10) {
+            String nome = "Pessoa " + (pessoasAdicionadas + 1);
+            int idade = random.nextInt(100) + 1;
+            boolean cadeirante = random.nextBoolean();
+            int peso = random.nextInt(100) + 50;
+            int andarDestino = random.nextInt(10);
+
+            Pessoa pessoa = new Pessoa(nome, idade, cadeirante, peso, andarDestino);
+            boolean adicionada = false;
+
+            for (Elevador elevador : elevadores) {
+                if (elevador.adicionarPessoa(pessoa)) {
+                    adicionada = true;
+                    pessoasAdicionadas++;
+                    break;
+                }
+            }
+
+            if (!adicionada) {
+                System.out.println("Não foi possível adicionar " + pessoa.getNome() + ": excede limite de peso ou capacidade.");
+            }
+        }
     }
 }
