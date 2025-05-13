0404d57249838fce89d30f8841a04f783844ee70
J Lucas Veloso <jlvelosofurtado2@gmail.com>
2025-05-13 10:31:05 -0300

    Update and rename Andar.java to Configuracao.java

---
+++ Configuracao.java
@@ -0,0 +1,11 @@
+public class Configuracao {
+    public int numeroAndares = 10;
+    public int numeroElevadores = 2;
+    public int capacidadeElevador = 10; // Capacidade máxima de passageiros
+    public int tempoMinimoViagem = 5;
+    public int tempoMaximoViagem = 10;
+    public int tempoMaximoEspera = 30;
+    public int consumoEnergiaDeslocamento = 2;
+    public int consumoEnergiaParada = 1;
+    public int pesoMaximoElevador = 1000;
+}
