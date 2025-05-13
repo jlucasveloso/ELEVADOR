636af31a8dca1c06eb6df098eff13a39ab5bb945
J Lucas Veloso <jlvelosofurtado2@gmail.com>
2025-05-13 10:52:35 -0300

    PainelControle.java

--- PainelControle.java
+++ PainelControle.java
@@ -2,7 +2,7 @@ public class PainelControle {
     private boolean[] botoes;
 
     public PainelControle() {
-        this.botoes = new boolean[10]; // Supondo 10 andares
+        this.botoes = new boolean[10];
     }
 
     public void selecionarAndar(int andar) {
@@ -11,16 +11,13 @@ public class PainelControle {
         }
     }
 
-    public boolean isAndarSelecionado(int andar) {
-        if (andar >= 0 && andar < botoes.length) {
-            return botoes[andar];
-        }
-        return false;
-    }
-
     public void resetarAndar(int andar) {
         if (andar >= 0 && andar < botoes.length) {
             botoes[andar] = false;
         }
+    }
+
+    public boolean isAndarSelecionado(int andar) {
+        return andar >= 0 && andar < botoes.length && botoes[andar];
     }
 }
